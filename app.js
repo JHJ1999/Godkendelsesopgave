bc5ad44ab60f42c81209dc98364f2f412c4f8f9f
JHJ1999 <jacobhejboel@gmail.com>
2020-10-12 12:55:29 +0200

    gem

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {res.send('Hello World!')})
+
+
+app.get('/user', (req, res) => {
+    res.json({"message": "user1"});
+})
+
+app.get('/paymentuser', (req, res) => {
+    res.json({"message": "user2"});
+})
+
+
 app.listen(port, 
-    () => {console.log(`Simple Express app listening on port ${port}!`)})
+    () => {console.log(`Express app listening on port ${port}!`)})
     
 
     console.log();
