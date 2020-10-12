fe963cbb9bcc7c69e0677633453688424b04fa4b
JHJ1999 <jacobhejboel@gmail.com>
2020-10-12 14:21:52 +0200

    users

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const users = require("./Routes/Users.js")
+const match = require("./Routes/Interest.js")
+const match = require("./Routes/Match.js")
+
+
+app.use("/Users", users);
+app.use("/Interest", interest);
+app.use("/Match", match)
+
+
 
 app.get('/', (req, res) => {res.send('Hello World!')})
 
 
-app.get('/user', (req, res) => {
-    res.json({"message": "user1"});
-})
 
-app.get('/paymentuser', (req, res) => {
-    res.json({"message": "user2"});
-})
+app
+.route("/paymentuser")
+.get( (req, res) => {})
+.post( (req, res) => {})
+.put( (req, res) => {})
+.delete( (req, res) =>{});
 
+
+app
+.route('/freeuser')
+.get( (req, res) => {})
+.post( (req, res) => {})
+.put( (req, res) => {})
+.delete( (req, res) => {});
 
 app.listen(port, 
     () => {console.log(`Express app listening on port ${port}!`)})
